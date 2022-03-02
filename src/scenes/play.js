e953743f47fc1e7fec5531fef7602e1547a25946
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-03-02 14:57:42 +0100

    Player Platform Colliding
    added collider for player and platforms

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -9,7 +9,9 @@ class Play extends Phaser.Scene {
     const map = this.createMap();
     const layers = this.createLayers(map);
 
-    this.createPlayer();
+    const player = this.createPlayer();
+
+    this.physics.add.collider(player, layers.platforms);
   }
 
   createMap(){
@@ -21,14 +23,19 @@ class Play extends Phaser.Scene {
   const tileset = map.getTileset("main_lev_build_1");
   const environment = map.createStaticLayer("environment", tileset);
   const platforms = map.createStaticLayer("platforms", tileset);
+  
+  platforms.setCollisionByExclusion(-1, true);
+  
   return {environment, platforms};
   }
 
   createPlayer(){
       const player = this.physics.add.sprite(100,250, "player");
       player.body.setGravityY(500);
-      player.setCollideWorldBounds(true);
+      player.setCollideWorldBounds(true);  
+      return player;
   }
+
 }
 
 export default Play;
