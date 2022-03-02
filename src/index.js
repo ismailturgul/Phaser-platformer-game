4bd0290e7670a9ca5ca01d141faaa5dd3ae9060d
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-03-02 13:31:13 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Phaser from "phaser";
+import PlayScene from "./scenes/play";
+
+const WIDTH = 1280;
+const HEIGHT = 600;
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+};
+
+const Scenes = [PlayScene];
+const createScene = (Scene) => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
+
+const config = {
+  type: Phaser.AUTO,
+  ...SHARED_CONFIG,
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {},
+  },
+  scene: initScenes(),
+};
+new Phaser.Game(config);
