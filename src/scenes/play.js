184956dbf0722526913e860ac0af6dba3042404b
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-03-02 17:01:53 +0100

    Player Movement
    added Movement to the player left and right

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,39 +4,55 @@ class Play extends Phaser.Scene {
   constructor() {
     super("PlayScene");
   }
-  
+
   create() {
     const map = this.createMap();
     const layers = this.createLayers(map);
 
-    const player = this.createPlayer();
-
-    this.physics.add.collider(player, layers.platformColliders);
+    this.player = this.createPlayer();
+    
+    this.playerSpeed = 200;
+    this.physics.add.collider(this.player, layers.platformColliders);
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  createMap(){
+  createMap() {
     const map = this.make.tilemap({ key: "map" });
     map.addTilesetImage("main_lev_build_1", "tiles-1");
     return map;
   }
-  createLayers(map){
-  const tileset = map.getTileset("main_lev_build_1");
-  const platformColliders = map.createStaticLayer("platforms_colliders", tileset)
-  const environment = map.createStaticLayer("environment", tileset);
-  const platforms = map.createStaticLayer("platforms", tileset);
-  
-  platformColliders.setCollisionByExclusion(-1, true);
-  
-  return {environment, platforms, platformColliders};
+  createLayers(map) {
+    const tileset = map.getTileset("main_lev_build_1");
+    const platformColliders = map.createStaticLayer(
+      "platforms_colliders",
+      tileset
+    );
+    const environment = map.createStaticLayer("environment", tileset);
+    const platforms = map.createStaticLayer("platforms", tileset);
+
+    platformColliders.setCollisionByProperty({ collides: true }, -1, true);
+
+    return { environment, platforms, platformColliders };
   }
 
-  createPlayer(){
-      const player = this.physics.add.sprite(100,250, "player");
-      player.body.setGravityY(500);
-      player.setCollideWorldBounds(true);  
-      return player;
+  createPlayer() {
+    const player = this.physics.add.sprite(100, 250, "player");
+    player.body.setGravityY(500);
+    player.setCollideWorldBounds(true);
+    return player;
   }
 
+  update(){
+      const { left, right } = this.cursors;
+      
+      if (left.isDown){
+        this.player.setVelocityX(-this.playerSpeed)
+      }else if (right.isDown){
+        this.player.setVelocityX(this.playerSpeed)
+      } else{
+        this.player.setVelocityX(0)
+      }
+  }
 }
 
 export default Play;
