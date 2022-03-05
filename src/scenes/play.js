11caef5ee357fceb8178c84dff7dc562431624b1
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-03-05 23:19:43 +0100

    Update play.js
    added Endzone and Overlap

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -19,6 +19,7 @@ class Play extends Phaser.Scene {
       },
     });
 
+    this.createEndOfLevel(playerZones.end, player);
     this.setupFollowupCameraOn(player);
   }
 
@@ -41,18 +42,18 @@ class Play extends Phaser.Scene {
     return { environment, platforms, platformColliders, playerZones };
   }
 
-  createPlayer({start}) {
+  createPlayer({ start }) {
     return new Player(this, start.x, start.y);
   }
   createPlayerColliders(player, { colliders }) {
     player.addCollider(colliders.platformColliders);
   }
   setupFollowupCameraOn(player) {
-    const { height, width, mapOffset, zommFactor } = this.config;
+    const { height, width, mapOffset, zoomFactor } = this.config;
     this.physics.world.setBounds(0, 0, width + mapOffset, height + 100);
     this.cameras.main
       .setBounds(0, 0, width + mapOffset, height)
-      .setZoom(zommFactor);
+      .setZoom(zoomFactor);
     this.cameras.main.startFollow(player);
   }
   getPlayerZones(playerZonesLayer) {
@@ -61,6 +62,18 @@ class Play extends Phaser.Scene {
       start: playerZones.find((zone) => zone.name === "startZone"),
       end: playerZones.find((zone) => zone.name === "endZone"),
     };
+  }
+
+  createEndOfLevel(end, player) {
+    const endOfLevel = this.physics.add
+      .sprite(end.x, end.y, "end")
+      .setAlpha(0)
+      .setSize(5, 200)
+      .setOrigin(0.5, 1);
+
+    this.physics.add.overlap(player, endOfLevel, () => {
+      console.log("player has won!");
+    });
   }
 }
 
