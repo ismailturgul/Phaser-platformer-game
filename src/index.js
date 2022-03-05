398c2015c570ece0f0437619c6b10f2d87941992
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-03-05 21:17:06 +0100

    Adding Camara setting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import Phaser from "phaser";
 import PlayScene from "./scenes/play";
 import PreloadScene from "./scenes/Preload";
 
-const WIDTH = 1280;
+const MAP_WIDTH = 1600;
+
+const WIDTH = document.body.offsetWidth;
 const HEIGHT = 600;
 
 const SHARED_CONFIG = {
+  mapOffset: MAP_WIDTH > WIDTH ? MAP_WIDTH - WIDTH : 0,
   width: WIDTH,
   height: HEIGHT,
 };
